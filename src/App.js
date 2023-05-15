ba2b632d94db1ab0832096467a57a2e21fe72d74
adeepak <adeepak@oritsl.com>
2023-05-15 11:14:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./componants/Home";
+import Contact from "./componants/ContactUs";
+import About from "./componants/AboutUs";
+import Event from "./componants/Events";
+import Login from "./componants/Login";
+import Register from "./componants/Register";
+import MembersPanel from "./componants/MembersPanel";
+import ResetPass from "./componants/ResetPass";
+import AdminPanal from "./componants/AdminPanal";
+import AdminHome from "./componants/AdminHome";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./componants/NavApp";
+
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
+  const isLoggedIn = window.localStorage.getItem("loggedIn");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="events" element={<Event />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Register" element={<Register />} />
+          <Route path="MembersPanel" element={isLoggedIn=="true" ? <MembersPanel /> : <Login/>} />
+          <Route path="ResetPass" element={<ResetPass />} />
+          <Route path="AdminPanal" element={<AdminPanal />} />
+          <Route path="AdminHome" element={<AdminHome />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
